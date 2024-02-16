409c263dd3afa7182ca3756b70f9e5967c5f564a
Yashaswini Sampath Kumar <44898518+YashaswiniSampath@users.noreply.github.com>
2024-02-15 20:30:55 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
+// const express = require('express');
+// const cors = require('cors');
+// const app = express();
+// app.use(cors());
+// const port = 3000;
+// app.get('/add/:number1/:number2', (req, res) => {
+//     const number1 = parseFloat(req.params.number1);
+//     const number2 = parseFloat(req.params.number2);
+//     const sum = number1 + number2;
+    
+//     res.json({ result: sum });
+// });
+
+// app.get('/calculate/:num1/:num2/:operator', (req, res) => {
+//     const number1 = parseFloat(req.params.num1);
+//     const number2 = parseFloat(req.params.num2);
+//     const operator = operator;
+//     const result=0;
+//     switch(operator) {
+//         case '+':
+//             result = num1 + num2;
+//             break;
+//         case '-':
+//             result = num1 - num2;
+//             break;
+//         case '*':
+//             result = num1 * num2;
+//             break;
+//         case '/':
+//             result = num1 / num2;
+//             break;
+//         default:
+//             result = "Invalid operator";
+//     }
+//     res.json({ result: sum });
+// });
+
+// app.get('/',(req,res)=>{
+//     res.send('Arithmetic service-Hello World!');
+// });
+
+
+
+// app.listen(port)
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
