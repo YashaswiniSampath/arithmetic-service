08c5fbab4ccb23ee2aae3a5bca8315cfaceb19e3
Yashaswini Sampath Kumar <44898518+YashaswiniSampath@users.noreply.github.com>
2024-02-15 20:11:46 -0500

    Add files via upload

--- src/test/arithmetica.test.js
+++ src/test/arithmetica.test.js
@@ -1,1 +1,13 @@
+const { add } = require("../arithmetica")
 
+test('2+3 is equal to 5', () =>{
+    expect(add(2, 3)).toBe(5)
+});
+
+test('2+7 is equal to 9', () =>{
+    expect(add(2, 7)).toBe(9)
+});
+
+test('12+71 is equal to 83', () =>{
+    expect(add(12, 71)).toBe(83)
+});
